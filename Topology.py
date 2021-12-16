a1e1cb53968b922a17977a147efec62bee150d0d
Mauricio Palma <mauricio.gpalma@gmail.com>
2021-12-16 16:21:59 -0300

    Kahuna Version 1.0

--- Topology.py
+++ Topology.py
@@ -147,7 +147,7 @@ class Topology:
                         #print("READY")
                         #print(bcolors.OKCYAN, end='');
                         #print(valid_matchesQ[i])
-                        print(valid_matchesQ[i].data_sent)
+                        #print(valid_matchesQ[i].data_sent)
                         #print(bcolors.ENDC, end='');
                         readyMatch = None;
                         for j in range(0, len(matchQ)):
@@ -342,6 +342,13 @@ class Topology:
                         #print(bcolors.OKCYAN, end='');
                         #print(valid_matchesQ[i])
                         #print(str(valid_matchesQ[i].data_sent) +  " --- " + str(valid_matchesQ[i].size))
+                        if round(valid_matchesQ[i].data_sent) -  (valid_matchesQ[i].size+16) != 0:
+                            if(valid_matchesQ[i].rankS != valid_matchesQ[i].rankR ):
+                                print(str(valid_matchesQ[i].rankS) + " -->" + str(valid_matchesQ[i].rankR))
+                                print("sent: " + str(valid_matchesQ[i].data_sent) +  " --- size " + str(valid_matchesQ[i].size + 16))
+                                print(round(valid_matchesQ[i].data_sent) -  (valid_matchesQ[i].size+16))
+                                print(valid_matchesQ[i].send_origin + " --- " + valid_matchesQ[i].recv_origin)
+                                #assert False, "send data was different from original data size"
                         #print(round(valid_matchesQ[i].data_sent) -  (valid_matchesQ[i].size+16))
                         #print(valid_matchesQ[i])
                         #print(bcolors.ENDC, end='');
@@ -613,6 +620,72 @@ class Topology:
                 #input("")
 
 
+
+    def __alg_SC_CC(self, matchQ, col_matchQ, currentPosition):
+        
+        valid_matchesQ : list[MQ_Match]; # For valid matches
+        valid_matchesQ = []
+        invalid_matchesQ : list[MQ_Match]; # For invalid matches
+        invalid_matchesQ = []
+        ###[1] Find the valid matches
+        # Valid matches are the ones that:
+        #       1) match their position on the "currentPosition" tracker of the messagequeue 
+        #       OR
+        #       2) the ones that are untrackable (negative tag)
+        for i in range(0, len(matchQ)):
+            thisMatch : MQ_Match = matchQ[i];
+            
+            if (
+                (    
+                    (thisMatch.positionS == currentPosition[thisMatch.rankS] or thisMatch.positionS < 0) and 
+                    (thisMatch.positionR == currentPosition[thisMatch.rankR] or thisMatch.positionR < 0)
+                ) or
+                (thisMatch.tag < 0)
+            ):
+                valid_matchesQ.append(thisMatch)
+            else:
+                invalid_matchesQ.append(thisMatch);
+        
+        # Valid among Collectives
+        for i in range(0, len(col_matchQ)):
+            tmp_valid, tmp_invalid = col_matchQ[i].getValidAndInvalidMatches();
+            valid_matchesQ = valid_matchesQ + tmp_valid;
+            invalid_matchesQ = invalid_matchesQ + tmp_invalid;
+        # We might be on a deadlock if there is no valid match on this point
+        assert len(valid_matchesQ) > 0, "No valid Match was found"
+        
+        
+        # Grab the earliest
+        earliest_match_index = self.findTheEarliestRequestIndex(valid_matchesQ, currentPosition);
+        
+        readyMatch = None;
+        for j in range(0, len(matchQ)):
+            if valid_matchesQ[earliest_match_index].id == matchQ[j].id:
+               readyMatch = matchQ.pop(j)
+               break;
+        if readyMatch is None:
+            for j in range(0, len(col_matchQ)):
+                readyMatch = col_matchQ[j].getMatchByID(valid_matchesQ[earliest_match_index].id);
+                if readyMatch is not None:
+                    break;
+        # If readyMatch is None, it does not exist... what happened?        
+        assert readyMatch is not None, "ready match is not presented on matches queues"
+        
+        
+        
+        #
+        # This is the actual SINGLE CHANNEL CIRCUIT SWITCHING
+        # Push forward everyone that shares communication with the earliest
+        for mi in range( len(matchQ) ):
+            inc = readyMatch.endCycle - matchQ[mi].baseCycle
+            if inc > 0:
+                matchQ[mi].baseCycle = matchQ[mi].baseCycle + inc;
+                matchQ[mi].endCycle = matchQ[mi].endCycle + inc;
+        
+        return readyMatch;
+
+
+
     # matchQ: matches queue
     # col_matchQ: Collective matches queue
     # currentPosition: vector with position of SEND/RECV for each rank
@@ -624,6 +697,9 @@ class Topology:
         
         if (self.topology == "KAHUNA"):
             return self.__alg__KAHUNA(matchQ, col_matchQ, currentPosition);
+
+        if (self.topology == "SC_CC"):
+            return self.__alg_SC_CC(matchQ, col_matchQ, currentPosition);
 
         print( bcolors.FAIL + "ERROR: Unknown topology " + self.topology + bcolors.ENDC);
         sys.exit(1);
